c3a73b4318a591f402cd5e29d33915b6c77467b7
akiraishu <akiraishu@users.noreply.github.com>
2019-09-20 14:42:50 -0400

    updated

---
+++ JS/script.js
@@ -0,0 +1,57 @@
+var shiftColor = function () {
+	var currentClassName = document.getElementById
+			('JSdiv').className;
+	if (currentClassName == 'JSdiv') {
+			document.getElementById('JSdiv').className 
+			= 'JSdiv white';
+		} else {
+			document.getElementById('JSdiv').className = 'JSdiv';
+		}
+
+}
+
+
+
+
+
+
+var bass = {
+					brand: 'epiphone',
+					model: 'thunderbird',
+					color: 'white',
+					isActive: false,
+					isPluggedIn: false,
+					numberOfStrings: 4,
+					strings: [
+						'string1', 
+						'string2', 
+						'string3', 
+						'string4'
+					],
+					plugIn: function () {
+						this.isPluggedIn = true;
+					},
+					plugOut: function () {
+						this.isPluggedIn = false,
+						alert('you plugged out the bass, dude!');
+					},	
+					listen: function (allGood) {
+						// console.log ('i can listen bass ' + allGood)
+						;
+						if (allGood == true) {
+							console.log ('i listen bass');
+							this.isPluggedIn = true,
+							alert('you plugged in the bass!');
+						} else {
+							console.log ('no bass here');
+							this.isPluggedIn = false;
+						}
+
+					}
+
+
+
+				};
+
+
+				console.log('javascript')
